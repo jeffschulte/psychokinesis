f8aa71880f84a4ebb3ff9326c96c9089855e5534
Jeffery Schulte <jeffschulte83@gmail.com>
2014-03-15 18:05:39 -0700

    added variables for player animation

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -36,9 +36,38 @@ SDL_Rect Animation::Get_Frame_to_Render(double y, double yvel, double height,
             //seperation of 70?
             return rect;
         }
+        // P_STAND=0, PUSH_R=1, PUSH_L=2, PUSH_U=3, PUSH_D=4,
+        //                 PUSH_R_F_STAND=5, STAND_F_PUSH_R=8, PUSH_L_F_STAND=11,
+        //                 STAND_F_PUSH_L=14, PUSH_U_F_STAND=17, STAND_F_PUSH_U=20,
+        //                 PUSH_D_F_STAND=23, STAND_F_PUSH_D=26, RUN_R=29, RUN_L=33,
+        //                 RUN_R_F_STAND=37, STAND_F_RUN_R=40, RUN_L_F_STAND=43,
+        //                 STAND_F_RUN_L=46, IN_AIR_L=49, IN_AIR_R=50, HGH_R=51,
+        //                 HGH_L=54, HGS_R=57, HGS_L=61, WALL_HUG_L=65,
+        //                 WALL_HUG_R=66
+        //player.yvel -= 2 * 9.8 * astate.targety * dt / 1000.0;
+        //player.xvel -= 2 * 9.8 * astate.targetx * dt / 1000.0;
+    //  double xcont;
+    // bool pushing;
+    //xcont, targetx targety are porportional to the joy stick movement itself
+
         else {
             last_frame_time = SDL_GetTicks();
-            //current_frame += 1;
+            if (mini_anim_frame == 0) {
+                if (y <= height/2 +.1) {
+                    current_state = ON_GROUND;
+                }
+                else if (y > height/2 + 3 || yvel > 0) {
+                    current_state = IN_AIR;
+                }
+                else {
+                    current_state = HITTING_GROUND;
+                }
+            }
+
+            current_frame += 1;
+            if (current_frame == 50){
+                current_frame = 2;
+            }
             SDL_Rect rect =  {68,22+(current_frame)*70,60,60};
             return rect;
         }
--- src/Animation.h
+++ src/Animation.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <SDL_image.h>
 #include "Logger.h"
+#include "Animation.h"
 
 class Animation {
 public:
@@ -16,6 +17,15 @@ int MaxFrames;
     enum EntType {BIG_MAN,LITTLE_MAN,PLAYER};
     int current_state;
     enum states {ON_GROUND,IN_AIR,HITTING_GROUND};
+    enum player_states {P_STAND=0, PUSH_R=1, PUSH_L=2, PUSH_U=3, PUSH_D=4,
+                        PUSH_R_F_STAND=5, STAND_F_PUSH_R=8, PUSH_L_F_STAND=11,
+                        STAND_F_PUSH_L=14, PUSH_U_F_STAND=17, STAND_F_PUSH_U=20,
+                        PUSH_D_F_STAND=23, STAND_F_PUSH_D=26, RUN_R=29, RUN_L=33,
+                        RUN_R_F_STAND=37, STAND_F_RUN_R=40, RUN_L_F_STAND=43,
+                        STAND_F_RUN_L=46, IN_AIR_L=49, IN_AIR_R=50, HGH_R=51,
+                        HGH_L=54, HGS_R=57, HGS_L=61, WALL_HUG_L=65,
+                        WALL_HUG_R=66};
+
     int mini_anim_frame;
     enum frames {STAND = 0, F_IN_AIR = 1, HIT_GROUND = 5, GET_UP =8};
 
